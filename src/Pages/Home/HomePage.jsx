27243c6c256f63227249081c6391aeb63fb6f740
pouriya moshfegh <pouriya.mosfeghs@gmail.com>
2023-05-07 19:55:09 +0430

    fix

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,5 +1,6 @@
 import Features from "../../Components/Features/Features"
 import Widget from "../../Components/Widget/Widget"
+
 import Userchart from "../../Components/userChart/userChart"
 export default function HomePage() {
   return (
