874bae5a8d4690d27def4023e433b9ffea0c3f03
pouriya moshfegh <pouriya.mosfeghs@gmail.com>
2023-05-30 13:37:21 +0330

    ai assistant starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import routes from "./Routes/Routes";
 import TopBar from "./Components/TopBar/TopBar";
 import { useRoutes } from "react-router-dom";
@@ -7,24 +6,32 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 import { MainContext } from "./contexts/MainContext";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import "./css/scroll.css";
+import { useNavigate } from 'react-router-dom';
+
+
 
 function App() {
-
- useEffect(() => {
-  alanBtn({
-      key: 'ee46d96470e10e9466118216852e693d2e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: (commandData) => {
-        if (commandData.command === 'go:back') {
-          // Call the client code that will react to the received command
-        }
-      }
-  });
-}, []);
-
+  const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState("");
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isDark, setIsDark] = useState(true);
-localStorage.clear()
+
+  useEffect(() => {
+    alanBtn({
+      key: "ee46d96470e10e9466118216852e693d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        commandData.command === "lite" && setIsDark(false);
+        commandData.command === "dark" && setIsDark(true);
+        commandData.command === "users" && navigate('/users', { replace: true });
+        commandData.command === "newUsers" && navigate('/newuser', { replace: true });
+        commandData.command === "products" && navigate('/products', { replace: true });
+        commandData.command === "home" && navigate('/', { replace: true });
+
+      },
+    });
+  }, []);
+
+  localStorage.clear();
 
   let router = useRoutes(routes);
   return (
@@ -38,10 +45,11 @@ localStorage.clear()
         setIsDark,
       }}
     >
+
       <div className={isDark ? "" : "dark"}>
         <TopBar />
         <Sidebar />
-        <div className="flex">
+           <div  className="flex ">
           <div className="nav-size"></div>
           <div className="right-side ">{router}</div>
         </div>
